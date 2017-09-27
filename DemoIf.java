34f37f9effbca56e3661e4df1853212ff704a555
azmiiiiiii <31796092+azmiiiiiii@users.noreply.github.com>
2017-09-27 14:32:08 +0700

    Add files via upload

---
+++ DemoIf.java
@@ -0,0 +1,9 @@
+public class DemoIf {
+		public static void main(String[] args) {
+			String nim="161530004";
+
+			if (nim=="161530004"){
+				System.out.println("Nama Anda Asifah Ulul Azmi");  
+			}
+		}
+}
